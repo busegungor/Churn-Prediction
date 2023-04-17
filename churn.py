853cbb839a95c6a40b28c6d571e445f889e6f5df
Buse Gungor <busegungor2303@gmail.com>
2023-04-17 22:05:41 +0300

    add tasks

---
+++ churn.py
@@ -0,0 +1,47 @@
+######################################
+# Telco Churn Prediction
+######################################
+
+#####################################
+# Business Problem
+#####################################
+# Şirketi terk edecek müşterileri tahmin edebilecek bir makine öğrenmesi modeli geliştirilmesi beklenmektedir.
+
+#####################################
+# Dataset History
+#####################################
+# Telco müşteri kaybı verileri, üçüncü çeyrekte Kaliforniya'daki 7043 müşteriye ev telefonu ve
+# İnternet hizmetleri sağlayan hayali bir telekom şirketi hakkında bilgi içerir.
+# Hangi müşterilerin hizmetlerinden ayrıldığını, kaldığını veya hizmete kaydolduğunu gösterir.
+
+###################################
+# TASKS
+###################################
+
+###################################
+# Görev 1 : Keşifçi Veri Analizi
+###################################
+
+# Adım 1: Numerik ve kategorik değişkenleri yakalayınız.
+# Adım 2: Gerekli düzenlemeleri yapınız. (Tip hatası olan değişkenler gibi)
+# Adım 3: Numerik ve kategorik değişkenlerin veri içindeki dağılımını gözlemleyiniz.
+# Adım 4: Kategorik değişkenler ile hedef değişken incelemesini yapınız.
+# Adım 5: Aykırı gözlem var mı inceleyiniz.
+# Adım 6: Eksik gözlem var mı inceleyiniz.
+
+###################################
+# Görev 2 : Feature Engineering
+###################################
+
+# Adım 1: Eksik ve aykırı gözlemler için gerekli işlemleri yapınız.
+# Adım 2: Yeni değişkenler oluşturunuz.
+# Adım 3: Encoding işlemlerini gerçekleştiriniz.
+# Adım 4: Numerik değişkenler için standartlaştırma yapınız.
+
+###################################
+# Görev 3 : Modelleme
+###################################
+
+# Adım 1: Sınıflandırma algoritmaları ile modeller kurup, accuracy skorlarını inceleyip. En iyi 4 modeli seçiniz.
+# Adım 2: Seçtiğiniz modeller ile hiperparametre optimizasyonu gerçekleştirin ve
+# bulduğunuz hiparparametreler ile modeli tekrar kurunuz.
